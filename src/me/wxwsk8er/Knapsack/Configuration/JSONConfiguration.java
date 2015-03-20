bbc0db3b45f255cbd9abb9fb79887a1ad510b2a3
BiggerOnTheInsideTeam <solidstudiosdevelopment@gmail.com>
2015-03-20 19:05:54 -0400

    Update JSONConfiguration.java

--- src/me/wxwsk8er/Knapsack/Configuration/JSONConfiguration.java
+++ src/me/wxwsk8er/Knapsack/Configuration/JSONConfiguration.java
@@ -2,7 +2,7 @@
 /** Copyright 2015 BiggerOnTheInside (development), all rights reserved.                                       */
 /** Released under the Binder License (https://github.com/BiggerOnTheInside/Licenses/blob/master/Binder.txt)   */
 /***************************************************************************************************************/
-
+ 
 package me.wxwsk8er.Knapsack.Configuration;
 
 import java.io.BufferedWriter;
